ab5f761b8b72d54f51c2cce0d36671d72e260a66
Joi Liu <547169735@qq.com>
2019-07-16 22:30:40 +0800

    test company findByCompanyName

--- src/main/java/com/tw/apistackbase/controller/CompanyController.java
+++ src/main/java/com/tw/apistackbase/controller/CompanyController.java
@@ -9,8 +9,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value="/companies")
+@RequestMapping("/companies")
 public class CompanyController {
+
   @Autowired
   private CompanyService companyService;
 
---
+++ src/test/java/com/tw/apistackbase/controller/CompanyControllerTest.java
@@ -0,0 +1,45 @@
+package com.tw.apistackbase.controller;
+
+import com.tw.apistackbase.model.Company;
+import com.tw.apistackbase.model.Employee;
+import com.tw.apistackbase.service.CompanyService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(CompanyController.class)
+class CompanyControllerTest {
+  @Autowired
+  private MockMvc mvc;
+
+  @MockBean
+  private CompanyService service;
+
+  @Test
+  void should_return_company_find_by_company_name() throws Exception {
+    Employee employee = new Employee(1, "Joi", 22, "female");
+    Company company = new Company("aaa", 10, Arrays.asList(employee));
+    when(service.findByCompanyName(anyString())).thenReturn(company);
+
+    ResultActions result = mvc.perform(get("/companies/{companyName}", company.getCompanyName()));
+
+    result.andExpect(status().isOk())
+        .andExpect(jsonPath("$.employeesNumber", is(10)));
+  }
+}
